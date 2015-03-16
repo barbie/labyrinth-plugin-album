5c39ebfa8f78af7eef6ef688762050a5b005d4bf
Barbie <barbie@missbarbell.co.uk>
2015-03-16 08:00:38 +0000

    upped version

--- lib/Labyrinth/Plugin/Album.pm
+++ lib/Labyrinth/Plugin/Album.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::Album;
 use strict;
 use warnings;
 
-our $VERSION = '1.11';
+our $VERSION = '1.12';
 
 =head1 NAME
 
--- lib/Labyrinth/Plugin/Album/Photos.pm
+++ lib/Labyrinth/Plugin/Album/Photos.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::Album::Photos;
 use strict;
 use warnings;
 
-our $VERSION = '1.11';
+our $VERSION = '1.12';
 
 =head1 NAME
 
